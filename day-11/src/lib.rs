9f0d35da9c3d921e53cf4a73654f4fc43750770c
Vlad Lytvynenko <sir.sagramor@gmail.com>
2022-12-11 22:04:46 +0100

    Add day-10, part 2

--- day-11/src/lib.rs
+++ day-11/src/lib.rs
@@ -9,32 +9,32 @@ use nom::{
 
 #[derive(Debug)]
 enum Operation {
-    Add(i32),
-    Multiply(i32),
+    Add(u64),
+    Multiply(u64),
     Square,
 }
 
 #[derive(Debug)]
 struct MonkeyTest {
-    divisible_by: i32,
+    divisible_by: u64,
     throw_true: usize,
     throw_false: usize,
 }
 
 #[derive(Debug)]
 struct Monkey {
-    items: Vec<i32>,
+    items: Vec<u64>,
     operation: Operation,
     test: MonkeyTest,
 }
 
 fn parse_test(input: &str) -> IResult<&str, MonkeyTest> {
-    let (input, divisible_by) = preceded(tag("  Test: divisible by "), complete::i32)(input)?;
+    let (input, divisible_by) = preceded(tag("  Test: divisible by "), complete::u64)(input)?;
     let (input, _) = newline(input)?;
-    let (input, throw_true) = preceded(tag("    If true: throw to monkey "), complete::u32)(input)?;
+    let (input, throw_true) = preceded(tag("    If true: throw to monkey "), complete::u64)(input)?;
     let (input, _) = newline(input)?;
     let (input, throw_false) =
-        preceded(tag("    If false: throw to monkey "), complete::u32)(input)?;
+        preceded(tag("    If false: throw to monkey "), complete::u64)(input)?;
     Ok((
         input,
         MonkeyTest {
@@ -47,8 +47,8 @@ fn parse_test(input: &str) -> IResult<&str, MonkeyTest> {
 
 fn parse_operation(input: &str) -> IResult<&str, Operation> {
     alt((
-        separated_pair(tag("+"), tag(" "), complete::i32).map(|(_, num)| Operation::Add(num)),
-        separated_pair(tag("*"), tag(" "), complete::i32).map(|(_, num)| Operation::Multiply(num)),
+        separated_pair(tag("+"), tag(" "), complete::u64).map(|(_, num)| Operation::Add(num)),
+        separated_pair(tag("*"), tag(" "), complete::u64).map(|(_, num)| Operation::Multiply(num)),
         separated_pair(tag("*"), tag(" "), tag("old")).map(|(_, _)| Operation::Square),
     ))(input)
 }
@@ -59,7 +59,7 @@ fn parse_monkey(input: &str) -> IResult<&str, Monkey> {
 
     let (input, items) = preceded(
         tag("  Starting items: "),
-        separated_list1(tag(", "), complete::i32),
+        separated_list1(tag(", "), complete::u64),
     )(input)?;
     let (input, _) = newline(input)?;
 
@@ -83,14 +83,15 @@ fn parse_monkeys(input: &str) -> IResult<&str, Vec<Monkey>> {
 }
 
 impl Monkey {
-    fn throw_items(&mut self, with_relief: bool) -> Vec<(i32, usize)> {
-        let mut to_throw: Vec<(i32, usize)> = vec![];
+    fn throw_items(&mut self, with_relief: bool, least_common_multiple: u64) -> Vec<(u64, usize)> {
+        let mut to_throw: Vec<(u64, usize)> = vec![];
         for item in &self.items {
             let mut new_item = match self.operation {
                 Operation::Add(n) => item + n,
                 Operation::Multiply(n) => item * n,
                 Operation::Square => item * item,
             };
+            new_item %= least_common_multiple;
             if with_relief {
                 new_item /= 3;
             }
@@ -109,43 +110,47 @@ impl Monkey {
 pub fn process_part1(input: &str) -> String {
     let (_, mut monkeys) = parse_monkeys(input).unwrap();
 
-    let mut item_counts: Vec<u32> = vec![0; monkeys.len()];
+    // since we have all prime numbers in input, this would work
+    let least_common_multiple = monkeys
+        .iter()
+        .map(|monkey| monkey.test.divisible_by)
+        .product::<u64>();
+
+    let mut item_counts: Vec<u64> = vec![0; monkeys.len()];
     for _ in 0..20 {
         for i in 0..monkeys.len() {
-            item_counts[i] += monkeys[i].items.len() as u32;
-            for (item, to_monkey) in monkeys[i].throw_items(true) {
+            item_counts[i] += monkeys[i].items.len() as u64;
+            for (item, to_monkey) in monkeys[i].throw_items(true, least_common_multiple) {
                 monkeys[to_monkey].items.push(item);
             }
         }
     }
 
     item_counts.sort_by(|a, b| b.cmp(a));
-    item_counts
-        .iter()
-        .take(2)
-        .fold(1, |acc, e| acc * e)
-        .to_string()
+    item_counts.iter().take(2).product::<u64>().to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
     let (_, mut monkeys) = parse_monkeys(input).unwrap();
 
-    let mut item_counts: Vec<u32> = vec![0; monkeys.len()];
+    // since we have all prime numbers in input, this would work
+    let least_common_multiple = monkeys
+        .iter()
+        .map(|monkey| monkey.test.divisible_by)
+        .product::<u64>();
+
+    let mut item_counts: Vec<u64> = vec![0; monkeys.len()];
     for _ in 0..10000 {
         for i in 0..monkeys.len() {
-            item_counts[i] += monkeys[i].items.len() as u32;
-            for (item, to_monkey) in monkeys[i].throw_items(false) {
+            item_counts[i] += monkeys[i].items.len() as u64;
+            for (item, to_monkey) in monkeys[i].throw_items(false, least_common_multiple) {
                 monkeys[to_monkey].items.push(item);
             }
         }
     }
 
     item_counts.sort_by(|a, b| b.cmp(a));
-    item_counts
-        .iter()
-        .take(2)
-        .fold(1, |acc, e| acc * e)
-        .to_string()
+    item_counts.iter().take(2).product::<u64>().to_string()
 }
 
 #[cfg(test)]
